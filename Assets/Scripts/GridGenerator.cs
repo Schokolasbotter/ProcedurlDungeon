4faf6b17302c3a06e38529051209788659133edd
Schokolasbotter <klela095@hotmail.com>
2024-02-08 11:19:42 +0000

    Random Walkers  Finished
    Finished the Random Grid Generations with Random Walkers

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,8 +14,7 @@ public class GridGenerator : MonoBehaviour
     private float meshHeight;
     public GameObject basicFloorTile;
     public GameObject floorTile;
-    private RandomWalker randomWalker;
-    private float timer = 0;
+    public List<RandomWalker> randomWalkers;
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +23,11 @@ public class GridGenerator : MonoBehaviour
         meshWidth = basicFloorTile.GetComponent<MeshRenderer>().bounds.size.x;
         meshHeight = basicFloorTile.GetComponent<MeshRenderer>().bounds.size.z;
         //Create Grid
-
+        gridSize = (int)Random.Range(25, 60);
         grid = new bool[gridSize, gridSize];
         gridObjects = new GameObject[gridSize, gridSize];
+        //Create RandomWalker List
+        randomWalkers = new List<RandomWalker>();
 
         //Visualize Grid
         for (int i = 0; i < gridSize; i++)
@@ -38,54 +40,80 @@ public class GridGenerator : MonoBehaviour
             }
         }
 
-        randomWalker = new RandomWalker(gridSize);
+        //Guaranteed 5 Walkers
+        randomWalkers.Add(new RandomWalker(0, gridSize/2, gridSize, grid));
+        randomWalkers.Add(new RandomWalker(gridSize/2, 0, gridSize, grid));
+        randomWalkers.Add(new RandomWalker(gridSize-1, gridSize/2, gridSize, grid));
+        randomWalkers.Add(new RandomWalker(gridSize/2, gridSize-1, gridSize, grid));
+        randomWalkers.Add(new RandomWalker(gridSize/2, gridSize/2, gridSize, grid));
+        
+        for(int i = 31; i  <= gridSize; i+= 10)
+        {
+            int randomX = (int)(Random.value * gridSize);
+            int randomY = (int)(Random.value * gridSize);
+            randomWalkers.Add(new RandomWalker(randomX, randomY, gridSize, grid));
+
+        }
+
+        bool finishedSteps = true;
+        while(finishedSteps)
+        {
+            //Move the walkers
+            for (int i = randomWalkers.Count - 1; i >= 0; i--)
+            {
+                if (randomWalkers[i].stepCount < 100 && randomWalkers[i] != null)
+                { 
+                    randomWalkers[i].moveOnGrid();
+                    if (randomWalkers[i].stepCount == 51 && Random.value < 1.1f && randomWalkers.Count < gridSize)
+                    {
+                        randomWalkers.Add(new RandomWalker((int)randomWalkers[i].position.x, (int)randomWalkers[i].position.y, gridSize, grid));
+                    }
+                }
+            }
+
+            finishedSteps &= randomWalkers[randomWalkers.Count - 1].stepCount < 100;
+        }
+
+        //Update the grid
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                if (grid[i, j] == true && gridObjects[i, j].tag != "Dungeon Floor")
+                {
+                    Vector3 tilePosition = gridObjects[i, j].transform.position;
+                    Destroy(gridObjects[i, j]);
+                    gridObjects[i, j] = Instantiate(floorTile, tilePosition, Quaternion.identity, transform);
+                }
+            }
+        }
+
+        randomWalkers.Clear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer > 0.5f && randomWalker != null)
-        {
-            randomWalker.moveOnGrid();
-            randomWalker.ActivatePosition(grid);
-            if(randomWalker.stepCount > 50)
-            {
-                randomWalker = null;
-            }
-            timer = 0;
-        }
-
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
-            {
-                if (grid[i,j] == true && gridObjects[i,j].tag != "Dungeon Floor")
-                {
-                    Vector3 tilePosition = gridObjects[i, j].transform.position;
-                    Destroy(gridObjects[i, j]);
-                    gridObjects[i,j] = Instantiate(floorTile, tilePosition , Quaternion.identity, transform);
-                }
-            }
-        }
-
-        timer += Time.deltaTime;
+               
     }
 
     public class RandomWalker
     {
         public Vector2 position;
         public int gridSize;
-        public int stepCount = 0;
-        public RandomWalker(int SizeOfGrid)
+        public bool[,] grid;
+        public int stepCount;
+
+        public RandomWalker(int inputX, int inputY, int SizeOfGrid, bool[,] gridObject)
         {
             gridSize = SizeOfGrid;
-            position.x = Random.Range(0, gridSize);
-            position.y = Random.Range(0, gridSize);
+            grid = gridObject;
+            position.x = inputX;
+            position.y = inputY;
         }
 
         public void moveOnGrid()
         {
-            Debug.Log("Walk " + Time.time);
             float randomValue = Random.value;
             switch (randomValue)
             {
@@ -101,16 +129,17 @@ public class GridGenerator : MonoBehaviour
                     if (position.y - 1 < 0) { break; }
                     position.y--;
                     break;
-                case < 1f:
+                case <= 1f:
                     if (position.y + 1 >= gridSize) { break; }
                     position.y++;
                     break;
             }
 
-            stepCount++;
+           stepCount++;
+           ActivatePosition();
         }
 
-        public void ActivatePosition(bool[,] grid)
+        public void ActivatePosition()
         {
             grid[(int)position.x, (int)position.y] = true;
         }
